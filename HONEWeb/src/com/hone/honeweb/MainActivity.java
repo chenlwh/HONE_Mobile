4d3bb7001fdd881084d2467d077fbf64e3e7faeb
peterwh83 <whgaos@cn.ibm.com>
2016-11-15 14:34:12 +0800

    t

--- HONEWeb/src/com/hone/honeweb/MainActivity.java
+++ HONEWeb/src/com/hone/honeweb/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	private TextView textView;
 	private TextView editView;
-// test by petergaoa
+// test by petergaoaa
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
