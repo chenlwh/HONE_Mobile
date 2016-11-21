90f20be1f8909007bd82d01e9cac37113ad862fb
chenlwh <chenlwh@cn.ibm.com>
2016-11-21 10:09:25 +0800

    formal codes for app

--- HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
+++ HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Welcome_Activity extends Activity {
+public class WelcomeActivity extends Activity {
 //Jane's page
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_welcome_);
+		setContentView(R.layout.activity_welcome);
 	}
 }
