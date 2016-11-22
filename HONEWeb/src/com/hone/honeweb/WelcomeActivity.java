907a98319a0accfa385890226717b03d8096ee95
Jane1016 <zjunwh@cn.ibm.com>
2016-11-22 15:31:44 +0800

    sample splash

--- HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
+++ HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
@@ -1,9 +1,9 @@
 package com.hone.honeweb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
 public class WelcomeActivity extends Activity {
 //Jane's page
@@ -11,5 +11,19 @@ public class WelcomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		
+		// 3 seconds redirect to teamactivety
+		new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(WelcomeActivity.this, TeamActivity.class);
+                startActivity(i);
+                //redirect to TeamActivity, then this page will disappear
+                finish();
+            }
+        }, 3000);
+		
+		
+		
 	}
 }
