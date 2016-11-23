d55c2de1f4ea71b09b3bb7866721a0912438e0b5
wendygao <gaozx@cn.ibm.com>
2016-11-23 16:09:15 +0800

    wendy version 2

--- HONEWeb/src/com/hone/honeweb/TeamActivity.java
+++ HONEWeb/src/com/hone/honeweb/TeamActivity.java
@@ -1,9 +1,13 @@
 package com.hone.honeweb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TeamActivity extends Activity {
 
@@ -11,8 +15,22 @@ public class TeamActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_team);
+		
+        Button nextpage = (Button) findViewById(R.id.button1);
+		
+		nextpage.setOnClickListener(nextpageListener);
 	}
 
+    private OnClickListener nextpageListener = new OnClickListener() {
+				
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent(TeamActivity.this,FunctionActivity.class);	    
+		    startActivity(intent);
+	    }
+    };
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
