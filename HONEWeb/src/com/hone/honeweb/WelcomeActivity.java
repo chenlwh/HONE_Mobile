7cd1bf3eacf95bf412b274739430b970cc8c38d7
Jane1016 <zjunwh@cn.ibm.com>
2016-11-25 16:40:16 +0800

    update with button click event

--- HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
+++ HONEWeb/src/com/hone/honeweb/WelcomeActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class WelcomeActivity extends Activity {
 //Jane's page
+	private Button SkipButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		SkipButton = (Button) findViewById(R.id.skip_button);
+		SkipButton.setOnClickListener(skipButtonlistener);
 		
-		// 3 seconds redirect to TeamActivety
+		// 6 seconds redirect to TeamActivety
 		new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,8 +28,18 @@ public class WelcomeActivity extends Activity {
                 //redirect to TeamActivity, then this page will disappear
                 finish();
             }
-        }, 3000);
+        }, 6000);
 		
 		
 	}
+	
+	private OnClickListener skipButtonlistener = new OnClickListener(){
+		@Override
+		public void onClick(View v) {
+				Intent skipToteam = new Intent(WelcomeActivity.this,TeamActivity.class);
+				startActivity(skipToteam);
+				finish();
+		}
+	};
+	
 }
