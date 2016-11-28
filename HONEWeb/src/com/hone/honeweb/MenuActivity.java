52b7b4871a7423482b89833c4ef02bf9371f70ca
chenlwh <chenlwh@cn.ibm.com>
2016-11-28 15:40:57 +0800

    menu

---
+++ HONEWeb/src/com/hone/honeweb/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.hone.honeweb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+	}
+}
