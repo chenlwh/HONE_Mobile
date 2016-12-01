b11074a3f3604e461a279539acbf9fae8b8e7b4d
chenlwh <chenlwh@cn.ibm.com>
2016-12-01 12:24:21 +0800

    Oops actions

--- HONEWeb/src/com/hone/honeweb/MenuActivity.java
+++ HONEWeb/src/com/hone/honeweb/MenuActivity.java
@@ -1,9 +1,11 @@
 package com.hone.honeweb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MenuActivity extends Activity {
 
@@ -11,5 +13,30 @@ public class MenuActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		ImageButton priceButton = (ImageButton) findViewById(R.id.price);
+		ImageButton orderButton = (ImageButton) findViewById(R.id.order);
+		ImageButton softwareButton = (ImageButton) findViewById(R.id.software);
+		ImageButton publicationsButton = (ImageButton) findViewById(R.id.publications);
+		
+		OnClickListener oopsClickListenter = new OopsClickListener();
+		
+		priceButton.setOnClickListener(oopsClickListenter);
+		orderButton.setOnClickListener(oopsClickListenter);
+		softwareButton.setOnClickListener(oopsClickListenter);
+		publicationsButton.setOnClickListener(oopsClickListenter);
+	}
+	
+	
+	class OopsClickListener implements OnClickListener{
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent(MenuActivity.this,OopsActivity.class);
+			startActivity(intent);
+			
+		}
+		
 	}
 }
+
